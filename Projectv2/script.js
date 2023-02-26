948b13d778e1810ba35f04104e2cfbb42467e70c
s00240122 <s00240122@atu.ie>
2023-02-26 20:03:29 +0000

    Initial commit

---
+++ Projectv2/script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    });
+});
+
+const hiddenElements = document.querySelector('.hidden');
+hiddenElements.forEach((element) => {
+    observer.observe(element);
+});
